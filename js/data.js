2625ca48584abfef42174b662ababa0b05451869
BulatCC <4134236@gmail.com>
2019-10-17 22:47:21 +0500

    11. Личный проект: модуляция

--- js/data.js
+++ js/data.js
@@ -15,38 +15,38 @@
   var MAX_Y = 630;
   var ADV_NUMBER = 8; //  количество объявлений
 
-// создает массив со случайнуми объектами (рандомное объявление)
-var createRandomAdv = function () {
-  var createAdv = [];
-  for (var i = 0; i < ADV_NUMBER; i++) {
-    createAdv[i] = {
-      author: {
-        avatar: 'img/avatars/user0' + (i + 1) + '.png'
-      },
-      offer: {
-        title: window.utils.createRandomElement(TITLE),
-        address: window.utils.createRandomElement(ADDRESS),
-        price: window.utils.createRandomElement(PRICE),
-        type: window.utils.createRandomElement(TYPE_OF_PLACE),
-        rooms: window.utils.createRandomElement(NUMBER_OF_ROOMS),
-        guests: window.utils.createRandomElement(GUESTS),
-        checkin: window.utils.createRandomElement(CHECKIN_CHECKOUT),
-        checkout: window.utils.createRandomElement(CHECKIN_CHECKOUT),
-        features: window.utils.createRandomArr(FEATURES),
-        description: window.utils.createRandomElement(DESCRIPTION),
-        photos: window.utils.createRandomArr(PHOTOS)
-      },
-      location: {
-        y: window.utils.createRandomNumber(MAX_Y, MIN_Y),
-        x: window.utils.createRandomXAxis(window.utils.X_AXIS)
-      }
-    };
-  }
-  return createAdv;
-};
+  // создает массив со случайнуми объектами (рандомное объявление)
+  var createRandomAdv = function () {
+    var createAdv = [];
+    for (var i = 0; i < ADV_NUMBER; i++) {
+      createAdv[i] = {
+        author: {
+          avatar: 'img/avatars/user0' + (i + 1) + '.png'
+        },
+        offer: {
+          title: window.utils.createRandomElement(TITLE),
+          address: window.utils.createRandomElement(ADDRESS),
+          price: window.utils.createRandomElement(PRICE),
+          type: window.utils.createRandomElement(TYPE_OF_PLACE),
+          rooms: window.utils.createRandomElement(NUMBER_OF_ROOMS),
+          guests: window.utils.createRandomElement(GUESTS),
+          checkin: window.utils.createRandomElement(CHECKIN_CHECKOUT),
+          checkout: window.utils.createRandomElement(CHECKIN_CHECKOUT),
+          features: window.utils.createRandomArr(FEATURES),
+          description: window.utils.createRandomElement(DESCRIPTION),
+          photos: window.utils.createRandomArr(PHOTOS)
+        },
+        location: {
+          y: window.utils.createRandomNumber(MAX_Y, MIN_Y),
+          x: window.utils.createRandomXAxis(window.utils.X_AXIS)
+        }
+      };
+    }
+    return createAdv;
+  };
 
-window.data = {
-  createRandomAdv: createRandomAdv,
-  ADV_NUMBER: ADV_NUMBER
-};
+  window.data = {
+    createRandomAdv: createRandomAdv,
+    ADV_NUMBER: ADV_NUMBER
+  };
 })();
