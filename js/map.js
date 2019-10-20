5863cb02f41d254bc3d88b63c56acc514a468c97
BulatCC <4134236@gmail.com>
2019-10-20 11:47:50 +0500

    выполнил 9 задание, карта криво работает

--- js/map.js
+++ js/map.js
@@ -1,7 +1,6 @@
 'use strict';
 
 (function () {
-  var ENTER_KEYCODE = 13;
   var INACTIVE_HALF_MAIN_PIN_SIZE = 32;
   var IS_PINS_RENDERED = false; // переменная для провеки условия отрисовки меток
 
@@ -35,14 +34,14 @@
   };
 
   // добавляет координаты карты в неактивном состоянии
-  addressField.setAttribute('placeholder', getCoordinatesPin(INACTIVE_HALF_MAIN_PIN_SIZE));
+  addressField.setAttribute('value', getCoordinatesPin(INACTIVE_HALF_MAIN_PIN_SIZE));
+
 
   // делает карту и форму активными, отрисовывает метки на карте
   var onShowMapAndForm = function () {
     // условие для однократной отрисовки меток на карте
     if (!IS_PINS_RENDERED) {
       createMapPins();
-      showOffer();
       map.classList.remove('map--faded');
       // записывает координаты метки в поле 'адрес'
       addressField.setAttribute('placeholder', getCoordinatesPin(window.utils.PIN_HEIGHT));
@@ -52,14 +51,20 @@
       }
       IS_PINS_RENDERED = true;
     }
+    // находит пины на карте, кроме основной метки
+    var mapPins = map.querySelectorAll('.map__pin:not(.map__pin--main)');
+    // выводит переменную в глобальную область видимости
+    window.mapPins = mapPins;
+    // отслеживает на какой пин был клик
+    onClickPin();
   };
 
   // активирует карту и форму по клику на основную метку
   mainPin.addEventListener('mousedown', onShowMapAndForm);
 
   // активирует карту и форму по нажатию enter на основную метку
-  mainPin.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
+  mainPin.addEventListener('keydown', function () {
+    if (window.utils.isEnterPressed) {
       onShowMapAndForm();
     }
   });
@@ -83,6 +88,50 @@
     var randomAdv = window.data.createRandomAdv();
     cardFragment.appendChild(window.card(randomAdv[0]));
     window.pin.similarAdv.appendChild(cardFragment);
+
+  };
+
+  // <-------------------- 9. доверяй, но проверяй -------------------->
+
+  // var onPinClick = function (evt) {
+  //   var target = evt.target;
+  //   var isContainMainPin = evt.target.parentElement.classList.contains('map__pin--main');
+  //   if (target.tagName === 'IMG' && !isContainMainPin) {
+  //     showOffer();
+  //   }
+  // }
+
+  // map.addEventListener('click', onPinClick);
+
+  // находит пин по которомы был клик
+  var onClickPin = function () {
+    var addClickListener = function (button) {
+      button.addEventListener('click', function () {
+        showOffer();
+        // обработчик нажатия esc для закрытия карточки
+        document.addEventListener('keydown', onEscClosePopup);
+        // обработчик нклика для закрытия карточки
+        var closePopup = map.querySelector('.popup__close');
+        closePopup.addEventListener('click', onClickClosePopup);
+      });
+    };
+    // находит пин по которму был клик
+    for (var j = 0; j < window.mapPins.length; j++) {
+      var button = window.mapPins[j];
+      addClickListener(button);
+      // onEscClosePopup(button);
+    }
+
+    // закрывает карточку объявления по нажатию на Esc
+    var onEscClosePopup = function () {
+      if (window.utils.isEscPressed) {
+        map.querySelector('.map__card').classList.add('hidden');
+      }
+    };
+    // закрывает карточку объявления по клику
+    var onClickClosePopup = function () {
+      map.querySelector('.map__card').classList.add('hidden');
+    };
   };
 
   // вынес в объект одно значение потомучто позже надо будет больше значений передавать в глобальную область видимости
